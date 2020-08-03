c5e6108c48a2bc3af17245ff959f3ba8fe110fa5
SagittariuX <aaronan1996@gmail.com>
2020-08-03 16:14:10 -0400

    fixed delete

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -1,4 +1,5 @@
 package LinkedList;
+import java.util.*;
 
 public class LinkedList {
     public Node head;
@@ -21,6 +22,97 @@ public class LinkedList {
         }
         tracker.next = new Node(data);
     }
+
+    public void push(int data){
+        Node n = new Node(data);
+        n.next = this.head;
+        this.head = n;
+    }
+
+
+    public Node get(int index){
+        Node n = this.head;
+        if(n == null){
+            System.out.println("empty list");
+            return null;
+        }
+        while( n != null && index > 0){
+            n = n.next;
+            index--;
+        }
+        if(index > 0){
+            System.out.println("out of index");
+        }
+
+        return n;
+    }
+
+    //remove by reference
+    public void remove(Node n){
+        if(n == null){ //out of index or doesnt exist
+            return;
+        }else if (n.next == null){//remove tail
+            removeTail();
+            return;
+        }
+        if(n == this.head){//remove head
+            this.head = this.head.next;
+        }else{//everything else
+            n.data = n.next.data;
+            n.next = n.next.next;
+        }
+        return;
+    }
+
+    //remove by index
+    public void remove(int index){
+        if(head == null){
+            System.out.println("empty list");
+            return;
+        }else if(index == 0){
+            this.head = this.head.next;
+            return;
+        }
+
+        Node n = this.head;
+        Node prev = null;
+        while( n != null && index > 0){
+            prev = n;
+            n = n.next;
+            index--;
+        }
+        if(index > 0){
+            System.out.println("out of index");
+            return;
+        }
+        if(n.next == null){//delete tail
+            prev.next = null;
+        }else{
+            n.data = n.next.data;
+            n.next = n.next.next;
+        }
+        return;
+    }
+
+    private void removeTail(){
+        Node n = this.head;
+    }
+
+    public void removeDuplicates(){
+        HashSet<Integer> set = new HashSet<Integer>();
+        Node n = this.head;
+
+        while(n != null){
+            if(set.contains(n.data)){
+                this.remove(n);    
+            }else{
+                set.add(n.data);
+            }
+            n = n.next;
+        }
+
+    }
+
 
     public void print(){
         Node tracker = this.head;
---
+++ SumBasicLinkedList.java
@@ -0,0 +1,30 @@
+import LinkedList.LinkedList;
+
+public class SumBasicLinkedList {
+    
+
+    //Input two linkedlists representing the reverse a int
+    //return the sum of the numbers Ex.
+    // L1 : 2 -> 3 -> 6     L2 : 3 -> 6 -> 9
+    // return:  1595
+    private int sumLists(LinkedList l1, LinkedList l2){
+        int sum = 0;
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+
+        LinkedList l1 = new LinkedList(2);
+        l1.add(3);
+        l1.add(6);
+
+        LinkedList l2 = new LinkedList(3);
+        l2.add(6);
+        l2.add(9);
+
+
+
+        
+    }
+}
