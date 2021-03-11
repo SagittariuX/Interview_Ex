46b3734445e2dc29b89ad2fdca625f511d5b0169
Aaron An <aaronan1996@gmail.com>
2021-03-11 12:35:24 -0500

    added codebyte ques

---
+++ LeetCodeJava/StarRating.java
@@ -0,0 +1,30 @@
+
+class StarRating{
+
+    static String starRating(String rating){
+        String ans = "";
+
+        int fullStar = Integer.parseInt(rating.substring(0, 1));
+        float halfStar = Float.parseFloat(rating.substring(1));
+
+        for(int i = 0 ; i < fullStar; i++){
+
+            ans += "full ";
+        }
+
+        if(halfStar > 0.74){
+            ans += "full ";
+            fullStar++;
+        }else if(halfStar > .24 && halfStar < .75){
+            ans += "half ";
+            fullStar++;
+        }
+
+        for(int i = fullStar; i < 5; i++){
+            ans += "empty ";
+        }
+
+        return ans;
+    }
+
+}
---
+++ LeetCodePython/SearchInsertPosition.py
@@ -0,0 +1,25 @@
+
+# https://leetcode.com/problems/search-insert-position/
+
+def naive_search(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: int
+    """
+
+    for i in range(len(nums)):
+        if target <= nums[i]: return i
+    
+    return len(nums)
+
+def binary_search(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: int
+    """
+    l , r = 0 , len(nums)
+
+    while l < r:
+         
