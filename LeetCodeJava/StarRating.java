a339d5c09103c1f7aa60ae76e50382b67ba92dfe
Aaron An <aaronan1996@gmail.com>
2021-03-11 12:37:23 -0500

    added codebyte ques

--- LeetCodeJava/StarRating.java
+++ LeetCodeJava/StarRating.java
@@ -8,9 +8,12 @@ class StarRating{
         float halfStar = Float.parseFloat(rating.substring(1));
 
         for(int i = 0 ; i < fullStar; i++){
-
             ans += "full ";
         }
+
+        if (fullStar == 5) return ans;
+
+
 
         if(halfStar > 0.74){
             ans += "full ";
